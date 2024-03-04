7a3ddc3ffdded7a0503d46f7b681f06364f83feb
aminought <aminought@gmail.com>
2024-03-05 00:19:08 +0500

    add settings

--- widgets.js
+++ widgets.js
@@ -100,6 +100,48 @@
             height: 100%;
         }
 
+        .WidgetSidebar {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            width: 300px;
+            transition: width 0.2s ease-out;
+            padding-left: 10px;
+        }
+
+        .WidgetSidebar.Hidden {
+            width: 0px;
+            padding-left: 0px;
+        }
+
+        .WidgetSettings {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+        }
+
+        .WidgetSidebar.Hidden .WidgetSettings {
+            display: none;
+        }
+
+        .WidgetInputRow {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: 5px;
+        }
+
+        .WidgetInputRow label {
+            width: 100%;
+            flex: 1;
+        }
+
+        .WidgetInputRow input {
+            width: 100%;
+            flex: 2;
+        }
+
         .WidgetWrapperDragArea {
             position: absolute;
             left: 0;
@@ -128,18 +170,58 @@
                         <path d="M18.65 10.9543C17.5938 9.12846 15.6197 7.90002 13.3586 7.90002C9.98492 7.90002 7.25 10.6349 7.25 14.0086C7.25 17.3823 9.98492 20.1172 13.3586 20.1172C15.1678 20.1172 16.7933 19.3308 17.9118 18.081" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
                     </svg>
                 </button>
+                <button class="WidgetToolbarButton WidgetToolbarSidebarButton">
+                    <svg width="20" height="20" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.25 14.5v.335l.25.224 1.622 1.452-1.334 2.187-1.685-.663-.38-.15-.332.238c-.41.294-.823.494-1.3.721l-.353.17-.063.385-.306 1.851h-2.635l-.416-1.89-.079-.357-.33-.159c-.477-.227-.89-.427-1.3-.721l-.332-.238-.38.15-1.685.663-1.321-2.167 1.498-1.147.295-.225v-2.318l-.295-.225-1.498-1.147 1.32-2.167 1.686.663.304.12.294-.14c.268-.129.528-.285.755-.42l.013-.008a8.21 8.21 0 0 1 .646-.362l.352-.168.064-.386.306-1.851h2.62l.306 1.851.064.386.352.168c.477.228.89.428 1.3.722l.332.238.38-.15 1.685-.663 1.324 2.17-1.458 1.15-.286.225V14.5z" stroke="currentColor" stroke-width="1.5"></path>
+                        <circle cx="14" cy="14" r="1.5" stroke="currentColor"></circle>
+                    </svg>
+                </button>
             </div>
         </div>
         <div class="WidgetRow">
             <div class="Widget">
                 <webview class="WidgetWebview"></webview>
             </div>
+            <div class="WidgetSidebar Hidden">
+                <div class="WidgetSettings">
+                    <div class="WidgetInputRow">
+                        <label>ID:</label>
+                        <input type="text" class="WidgetId">
+                    </div>
+                    <div class="WidgetInputRow">
+                        <label>URL:</label>
+                        <input type="text" class="WidgetUrl">
+                    </div>
+                    <div class="WidgetInputRow">
+                        <label>Zoom Factor:</label>
+                        <input type="number" class="WidgetZoomFactor" step="0.01" min="0" max="1">
+                    </div>
+                    <div class="WidgetInputRow">
+                        <label>Selector:</label>
+                        <input type="text" class="WidgetSelector">
+                    </div>
+                    <div class="WidgetInputRow">
+                        <label>Width (px):</label>
+                        <input type="number" class="WidgetWidth" step="1" min="100" max="10000">
+                    </div>
+                    <div class="WidgetInputRow">
+                        <label>Height (px):</label>
+                        <input type="number" class="WidgetHeight" step="1" min="100" max="10000">
+                    </div>
+                    <div class="WidgetInputRow">
+                        <label>Timeout (ms):</label>
+                        <input type="number" class="WidgetTimeout" step="100" min="0" max="10000">
+                    </div>
+                    <input type="submit" value="Save">
+                </div>
+            </div>
         </div>
     `;
 
     class Widgets {
         #db = new Database();
-        #widgets = null;
+        #widgets = WIDGETS;
+        #widgetsDivCache = null;
         #sdWrapperMutationObserver = null;
         #draggedWidgetWrapper = null;
 
@@ -190,38 +272,16 @@
             this.#reloadButton.addEventListener('click', () => this.#reloadWidgets());
         }
 
-        #createWidgetReloadButtonListener(button) {
+        #createWidgetReloadButtonListener(widgetWrapper, button) {
             button.addEventListener('click', () => {
-                const widgetWrapper = this.#findParentByClass(button, 'WidgetWrapper');
                 this.#reloadWidget(widgetWrapper);
             });
         }
 
-        // config
-
-        #compareWidgetOrders(configWidgetOrders, dbWidgetOrders) {
-            const configWidgetOrdersSet = new Set(configWidgetOrders.map((widgetOrder) => widgetOrder.id));
-            const dbWidgetOrdersSet = new Set(dbWidgetOrders.map((widgetOrder) => widgetOrder.id));
-            return this.#compareSets(configWidgetOrdersSet, dbWidgetOrdersSet);
-        }
-
-        #generateWidgetOrdersFromConfig() {
-            const widgetOrders = [];
-            WIDGETS.forEach((widgetInfo, index) => {
-                widgetOrders.push({order: index, id: widgetInfo.id})
+        #createWidgetSidebarButtonListener(widgetWrapper, button) {
+            button.addEventListener('click', () => {
+                this.#showWidgetSidebar(widgetWrapper);
             });
-            return widgetOrders;
-        }
-
-        #generateWidgetOrdersFromStartPage() {
-            const widgetOrders = [];
-            var index = 0;
-            for (const widgetWrapper of this.#widgets.children) {
-                const widget = widgetWrapper.querySelector('.Widget');
-                widgetOrders.push({order: index, id: widget.id})
-                index++;
-            }
-            return widgetOrders;
         }
 
         // builders
@@ -237,28 +297,29 @@
         }
 
         async #createWidgets() {
-            this.#widgets = document.createElement('div');
-            this.#widgets.className = 'Widgets';
+            this.#widgetsDivCache = document.createElement('div');
+            this.#widgetsDivCache.className = 'Widgets';
 
-            if (!this.#widgets) {
+            if (!this.#widgetsDivCache) {
                 this.#createWidgetsDelayed();
                 return;
             }
 
-            var widgetOrders = this.#generateWidgetOrdersFromConfig();
-            const dbWidgetOrders = await this.#db.getWidgetOrders();
-
-            if (!this.#compareWidgetOrders(widgetOrders, dbWidgetOrders)) {
-                this.#db.clearWidgetOrders();
-                this.#db.addWidgetOrders(widgetOrders);
+            const dbWidgets = await this.#db.getWidgets();
+            if (dbWidgets.length > 0) {
+                this.#widgets = dbWidgets;
             } else {
-                widgetOrders = dbWidgetOrders;
+                this.#widgets.forEach((widget, order) => {
+                    if (!('order' in widget)) {
+                        widget['order'] = order;
+                    }
+                });
+                await this.#db.addWidgets(this.#widgets);
             }
 
-            widgetOrders.forEach((widgetOrder) => {
-                const widgetInfo = WIDGETS.find((widgetInfo) => widgetInfo.id === widgetOrder.id);
+            this.#widgets.forEach((widgetInfo) => {
                 const widgetWrapper = this.#createWidgetWrapper(widgetInfo);
-                this.#widgets.appendChild(widgetWrapper);
+                this.#widgetsDivCache.appendChild(widgetWrapper);
             });
         }
 
@@ -279,6 +340,7 @@
             this.#configureWidget(widgetWrapper, widgetInfo);
             this.#configureWebview(widgetWrapper, widgetInfo);
             this.#configureWidgetToolbarReloadButton(widgetWrapper);
+            this.#configureWidgetToolbarSettingsButton(widgetWrapper);
 
             return widgetWrapper;
         }
@@ -300,13 +362,18 @@
 
         #configureWidgetToolbarReloadButton(widgetWrapper) {
             const reloadButton = widgetWrapper.querySelector('.WidgetToolbarReloadButton');
-            this.#createWidgetReloadButtonListener(reloadButton);
+            this.#createWidgetReloadButtonListener(widgetWrapper, reloadButton);
+        }
+
+        #configureWidgetToolbarSettingsButton(widgetWrapper) {
+            const sidebarButton = widgetWrapper.querySelector('.WidgetToolbarSidebarButton');
+            this.#createWidgetSidebarButtonListener(widgetWrapper, sidebarButton);
         }
 
         #createDragAndDropAreas() {
             const dragArea = this.#createWidgetDragArea();
             this.#draggedWidgetWrapper.appendChild(dragArea);
-            for (const widget of this.#widgets.children) {
+            for (const widget of this.#widgetsDiv.children) {
                 if (widget === this.#draggedWidgetWrapper) {
                     continue;
                 }
@@ -350,12 +417,12 @@
             if (START_PAGE_BUTTON && this.#activeStartPageButton.innerText != START_PAGE_BUTTON) {
                 return;
             }
-            this.#speedDial.appendChild(this.#widgets);
+            this.#speedDial.appendChild(this.#widgetsDivCache);
             this.#fixPointerEvents();
         }
 
         #fixPointerEvents() {
-            for (const widget of this.#widgets.children) {
+            for (const widget of this.#widgetsDiv.children) {
                 const webview = widget.querySelector('webview');
                 webview.style.pointerEvents = 'all';
             }
@@ -397,17 +464,26 @@
             });
         }
 
+        #reloadWidgets() {
+            if (!this.#widgetsDiv) {
+                return;
+            }
+            for (const widgetWrapper of this.#widgetsDiv.children) {
+                this.#reloadWidget(widgetWrapper);
+            }
+        }
+
         #reloadWidget(widgetWrapper) {
             const webview = widgetWrapper.querySelector('webview');
             webview.reload();
         }
 
-        #reloadWidgets() {
-            if (!this.#widgetsDiv) {
-                return;
-            }
-            for (const widgetWrapper of this.#widgets.children) {
-                this.#reloadWidget(widgetWrapper);
+        #showWidgetSidebar(widgetWrapper) {
+            const widgetSidebar = widgetWrapper.querySelector('.WidgetSidebar');
+            if (widgetSidebar.classList.contains('Hidden')) {
+                widgetSidebar.classList.remove('Hidden');
+            } else {
+                widgetSidebar.classList.add('Hidden');
             }
         }
 
@@ -416,6 +492,22 @@
                 dropArea.parentElement.removeChild(dropArea);
             }
             this.#dragArea?.parentElement?.removeChild(this.#dragArea);
+        }
+
+        #updateWidgets() {
+            const widgets = [];
+            var order = 0;
+            for (const widgetWrapper of this.#widgetsDiv.children) {
+                const widgetDiv = widgetWrapper.querySelector('.Widget');
+                const widget = this.#widgets.find(widget => widget.id == widgetDiv.id);
+                widget.order = order;
+                widgets.push(widget);
+                order++;
+            }
+            this.#widgets = widgets;
+            this.#db.clearWidgets().then(() => {
+                this.#db.addWidgets(this.#widgets);
+            });
         }
 
         // WidgetWrapper actions
@@ -427,7 +519,10 @@
 
         #hideWidgetHeader(widgetWrapper) {
             const header = widgetWrapper.querySelector('.WidgetHeader');
-            header.classList.add('Hidden');
+            const sidebar = widgetWrapper.querySelector('.WidgetSidebar');
+            if (sidebar.classList.contains('Hidden')) {
+                header.classList.add('Hidden');
+            }
         } 
 
         #dragWidgetWrapper(widgetWrapper) {
@@ -437,13 +532,10 @@
 
         #dropWidgetWrapper(targetWidgetWrapper) {
             if (targetWidgetWrapper != this.#draggedWidgetWrapper) {
-                this.#widgets.insertBefore(this.#draggedWidgetWrapper, targetWidgetWrapper);
+                this.#widgetsDiv.insertBefore(this.#draggedWidgetWrapper, targetWidgetWrapper);
             }
             this.#removeDragAndDropAreas();
-            const widgetOrders = this.#generateWidgetOrdersFromStartPage();
-            this.#db.clearWidgetOrders().then(() => {
-                this.#db.addWidgetOrders(widgetOrders);
-            });
+            this.#updateWidgets();
             return false;
         }
 
@@ -521,22 +613,34 @@
     class Database {
         #db = null;
         #dbName = 'Widgets';
-        #objectStoreName = 'order';
+        #orderObjectStoreName = 'order';
+        #widgetsObjectStoreName = 'widgets';
 
         connect() {
-            const request = window.indexedDB.open(this.#dbName);
+            const request = window.indexedDB.open(this.#dbName, 2);
             request.onerror = () => {
                 console.log('Failed to open database')
             };
             return new Promise((resolve, reject) => {
                 request.onupgradeneeded = (event) => {
                     this.#db = event.target.result;
-                    let objectStore = this.#db.createObjectStore(this.#objectStoreName, {
+
+                    if (!this.#db.objectStoreNames.contains(this.#orderObjectStoreName)) {
+                        let orderObjectStore = this.#db.createObjectStore(this.#orderObjectStoreName, {
+                            keyPath: 'order'
+                        });
+                        orderObjectStore.transaction.oncomplete = () => {
+                            console.log(`ObjectStore ${this.#orderObjectStoreName} created`);
+                        }
+                    }
+
+                    let widgetsObjectStore = this.#db.createObjectStore(this.#widgetsObjectStoreName, {
                         keyPath: 'order'
                     });
-                    objectStore.transaction.oncomplete = () => {
-                        console.log('ObjectStore created');
+                    widgetsObjectStore.transaction.oncomplete = () => {
+                        console.log(`ObjectStore ${this.#widgetsObjectStoreName} created`);
                     }
+
                     resolve(true);
                 };
                 request.onsuccess = (event) => {
@@ -552,9 +656,39 @@
 
         addWidgetOrders(widgetOrders) {
             if (!this.#db) return;
+            return this.#add(this.#orderObjectStoreName, widgetOrders);
+        }
 
-            const trx = this.#db.transaction(this.#objectStoreName, 'readwrite');
-            const objectStore = trx.objectStore(this.#objectStoreName);
+        addWidgets(widgets) {
+            if (!this.#db) return;
+            return this.#add(this.#widgetsObjectStoreName, widgets);
+        }
+
+        getWidgetOrders() {
+            if (!this.#db) return;
+            return this.#getAll(this.#orderObjectStoreName);
+        }
+
+        getWidgets() {
+            if (!this.#db) return;
+            return this.#getAll(this.#widgetsObjectStoreName);
+        }
+
+        clearWidgetOrders() {
+            if (!this.#db) return;
+            return this.#clearAll(this.#orderObjectStoreName);
+        }
+
+        clearWidgets() {
+            if (!this.#db) return;
+            return this.#clearAll(this.#widgetsObjectStoreName);
+        }
+
+        #add(objectStoreName, objects) {
+            if (!this.#db) return;
+
+            const trx = this.#db.transaction(objectStoreName, 'readwrite');
+            const objectStore = trx.objectStore(objectStoreName);
 
             return new Promise((resolve, reject) => {
                 trx.oncomplete = () => {
@@ -565,17 +699,17 @@
                     console.log('Not inserted');
                     resolve(false);
                 };
-                widgetOrders.forEach((widgetOrder) => {
-                    objectStore.add(widgetOrder);
+                objects.forEach((object) => {
+                    objectStore.add(object);
                 });
             });
         }
 
-        getWidgetOrders() {
+        #getAll(objectStoreName) {
             if (!this.#db) return;
 
-            const trx = this.#db.transaction(this.#objectStoreName, 'readonly');
-            const objectStore = trx.objectStore(this.#objectStoreName);
+            const trx = this.#db.transaction(objectStoreName, 'readonly');
+            const objectStore = trx.objectStore(objectStoreName);
 
             return new Promise((resolve, reject) => {
                 trx.oncomplete = () => {
@@ -593,11 +727,11 @@
             });
         }
 
-        clearWidgetOrders() {
+        #clearAll(objectStoreName) {
             if (!this.#db) return;
 
-            const trx = this.#db.transaction(this.#objectStoreName, 'readwrite');
-            const objectStore = trx.objectStore(this.#objectStoreName);
+            const trx = this.#db.transaction(objectStoreName, 'readwrite');
+            const objectStore = trx.objectStore(objectStoreName);
 
             return new Promise((resolve, reject) => {
                 trx.oncomplete = () => {
